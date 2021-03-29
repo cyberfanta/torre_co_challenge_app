b1d0a46c22c75b0f079b6e91ee40a2d1712d588d
Julio César León <julioleon2004@gmail.com>
2021-03-29 00:58:01 -0400

    Initial commit -- Beginning the view construction

--- app/src/main/java/com/cyberfanta/torre_co_challenge_app/views/MainActivity.kt
+++ app/src/main/java/com/cyberfanta/torre_co_challenge_app/views/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.cyberfanta.torre_co_challenge_app.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.cyberfanta.torre_co_challenge_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun defaultClickable(view: View) {
+        //todo: delete as soon as possible
+        Toast.makeText(applicationContext, view.id.toString(), Toast.LENGTH_SHORT).show()
     }
 }
